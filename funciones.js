7e19cf46b223825e397d3de5f331e28ccfb29a2d
Athilema <116764990+Athilema@users.noreply.github.com>
2023-02-25 21:47:42 -0300

    Update funciones.js

--- funciones.js
+++ funciones.js
@@ -2,3 +2,9 @@ document.getElementById("botonFormulario").onclick = function () {
 	document.getElementById("principal").style.display = "none";
 	document.getElementById("mensajeError").innerHTML = "Funcion no disponible. Comunicarse por el siguiente medio";
 }
+
+const boton = document.getElementById("botonFormulario");
+boton.addEventListener () {
+	document.getElementById("principal").style.display = "none";
+	document.getElementById("mensajeError").innerHTML = "Funcion no disponible. Comunicarse por el siguiente medio";
+}
