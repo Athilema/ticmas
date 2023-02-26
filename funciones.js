6194c5ec5d129e15f88d30c05fec7c3f0198e73f
Athilema <116764990+Athilema@users.noreply.github.com>
2023-02-26 13:56:40 -0300

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -2,3 +2,8 @@ document.getElementById("botonFormulario").onclick = function () {
 	document.getElementById("principal").style.display = "none";
 	document.getElementById("mensajeError").innerHTML = "Funcion no disponible. Comunicarse por el siguiente medio";
 }
+
+document.getElementById("botonGuias").onclick = function () {
+	document.getElementById("nav").style.border = "5px solid red";
+
+}
