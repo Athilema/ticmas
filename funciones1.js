e254279ef7087737eefbf2bd62c78519a9204aac
Athilema <116764990+Athilema@users.noreply.github.com>
2023-02-26 14:09:41 -0300

    Update funciones1.js

--- funciones1.js
+++ funciones1.js
@@ -1,4 +1,14 @@
 document.getElementById("botonGuias").onclick = function () {
-	document.getElementById("nav").style.border = "5px solid red";
+	document.getElementById("nav").style.border = "3px solid red";
+	document.getElementById("barramenu").style.border = "3px solid red";
+	document.getElementById("menu1").style.border = "3px solid red";
+	document.getElementById("menu2").style.border = "3px solid red";
+	document.getElementById("cuerpo").style.border = "3px solid red";
+	document.getElementById("superior").style.border = "3px solid red";
+	document.getElementById("titulo").style.border = "3px solid red";
+	document.getElementById("foto").style.border = "3px solid red";
+	document.getElementById("barra_lateral").style.border = "3px solid red";
+	document.getElementById("principal").style.border = "3px solid red";
+	
 
 }
